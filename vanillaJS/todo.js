943a8dd07211da29732f659b4743e8386cb58b3b
Moonsung <gmsj159@gmail.com>
2020-10-23 21:13:05 +1000

    DOM scrape function

--- vanillaJS/todo.js
+++ vanillaJS/todo.js
@@ -1,1 +1,18 @@
-console.log("test");
+var DOM = document.querySelector(".todolist");
+// Collect All child dom under the "dom" and return object 
+// Key is first class name
+// Value is DOM
+function _DOM_SCRAPE(dom){
+    var i;
+    var obj={};
+    if(!dom.hasChildNodes()){
+        return 0;
+    }
+    for(i=0; i < dom.children.length; i++){
+        Object.assign(obj,_DOM_SCRAPE(dom.children[i]))
+        obj[dom.children[i].classList[0]] = dom.children[i];
+    }
+    return obj;
+}
+var el = _DOM_SCRAPE(DOM);
+
