b0bf61c2355dafec2397b7f77746d5c88a8fdc3d
Moonsung <gmsj159@gmail.com>
2020-10-26 12:57:05 +1000

    NULL Filtering

--- vanillaJS/todo.js
+++ vanillaJS/todo.js
@@ -1,4 +1,4 @@
-var DOM = document.querySelector(".todolist");
+var todolist = document.querySelector('.todolist');
 // Collect All child dom under the "dom" and return object 
 // Key is first class name
 // Value is DOM
@@ -14,5 +14,53 @@ function _DOM_SCRAPE(dom){
     }
     return obj;
 }
-var el = _DOM_SCRAPE(DOM);
+function keyUpHandler(e){
+    if(e.keyCode === 13){
+        var el = document.activeElement;
+        var txt = this['text-input'];
+        if(el === txt){
+            if(txt.value !== ""){
+                // List Add Function!!
+                //
+                return 0;
+            }
+        }  
+    }
+}
+function _ADD_BTN(e){
+    console.log(this['text-input']);
+}
+function _LIST_BTN(e){
+    console.log(e.target);
+}
+function _LIST_INIT(dom){
+    var DOM = _DOM_SCRAPE(dom);
+    dom.addEventListener('keyup',keyUpHandler.bind(DOM),false);
+    DOM['btn-add'].addEventListener('click',_ADD_BTN.bind(DOM), false);
+
+    DOM['btn-list'].addEventListener('click',_LIST_BTN, false);
+    DOM['btn-complete-all'].addEventListener('click',_LIST_BTN, false);
+    DOM['all'].addEventListener('click',_LIST_BTN, false);
+    DOM['active'].addEventListener('click',_LIST_BTN, false);
+    DOM['completed'].addEventListener('click',_LIST_BTN, false);
+    DOM['btn-clear'].addEventListener('click',_LIST_BTN, false);
+}
+_LIST_INIT(todolist);
+
+//var DOM = _DOM_SCRAPE(todolist);
+
+/* var DOM = _DOM_SCRAPE(todolist);
+function ring(){
+    return this.all;
+}
+var tc = ring.bind(DOM);
+console.log(tc());
+function card(){
+    console.log(this.all);
+}
+var gc = card.bind(DOM);
+gc(); */
+
+
+
 
