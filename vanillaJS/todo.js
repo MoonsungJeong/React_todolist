363968753d0836963079889dcd9b777d462d010e
Moonsung <gmsj159@gmail.com>
2020-10-26 15:29:22 +1000

    Add list function

--- vanillaJS/todo.js
+++ vanillaJS/todo.js
@@ -1,10 +1,10 @@
-var todolist = document.querySelector('.todolist');
+const todolist = document.querySelector('.todolist');
 // Collect All child dom under the "dom" and return object 
 // Key is first class name
 // Value is DOM
 function _DOM_SCRAPE(dom){
-    var i;
-    var obj={};
+    let i;
+    let obj={};
     if(!dom.hasChildNodes()){
         return 0;
     }
@@ -14,27 +14,74 @@ function _DOM_SCRAPE(dom){
     }
     return obj;
 }
+function _ADD_BTN(e){
+    const txt = this['text-input'];
+    const num = this['count']++;
+    if(txt.value === ""){
+        return false;
+    } 
+    const node=`
+    <li class="${num} flex line-top">
+        <div class="btn-check _col_xs-10 _center"><i class="far fa-check-circle"></i></div>
+        <div class="bar-text _col_xs-80 _padding-left">${txt.value}</div>
+        <div class="btn-delete _col_xs-10 _center"><i class="fas fa-times"></i></div>
+    </li>
+    `
+    let li = document.createElement('li');
+    let div_1 = document.createElement('div');
+    let div_2 = document.createElement('div');
+    let div_3 = document.createElement('div');
+    let i_1 = document.createElement('i');
+    let i_3 = document.createElement('i');
+    
+    li.classList.add(num);
+    li.classList.add('flex');
+    li.classList.add('line-top');
+    div_1.classList.add('btn-check');
+    div_1.classList.add('_col_xs-10');
+    div_1.classList.add('_center');
+    div_2.classList.add('bar-text');
+    div_2.classList.add('_col_xs-80');
+    div_2.classList.add('_padding-left');
+    div_3.classList.add('btn-delete');
+    div_3.classList.add('_col_xs-10');
+    div_3.classList.add('_center');
+    i_1.classList.add('far');
+    i_1.classList.add('fa-check-circle');
+    i_3.classList.add('fas');
+    i_3.classList.add('fa-times');
+
+    div_1.appendChild(i_1);
+    div_2.innerHTML = txt.value;
+    div_3.appendChild(i_3);
+    li.appendChild(div_1);
+    li.appendChild(div_2);
+    li.appendChild(div_3);
+    this['list'].appendChild(li);
+    this[num] = li;
+    txt.value="";
+    txt.focus();
+}
 function keyUpHandler(e){
     if(e.keyCode === 13){
-        var el = document.activeElement;
-        var txt = this['text-input'];
+        const el = document.activeElement;
+        const txt = this['text-input'];
         if(el === txt){
             if(txt.value !== ""){
                 // List Add Function!!
-                //
+                _ADD_BTN.bind(this)();
                 return 0;
             }
         }  
     }
 }
-function _ADD_BTN(e){
-    console.log(this['text-input']);
-}
 function _LIST_BTN(e){
     console.log(e.target);
 }
 function _LIST_INIT(dom){
-    var DOM = _DOM_SCRAPE(dom);
+    const DOM = _DOM_SCRAPE(dom);
+    DOM['count']=0;
+
     dom.addEventListener('keyup',keyUpHandler.bind(DOM),false);
     DOM['btn-add'].addEventListener('click',_ADD_BTN.bind(DOM), false);
 
@@ -47,7 +94,7 @@ function _LIST_INIT(dom){
 }
 _LIST_INIT(todolist);
 
-//var DOM = _DOM_SCRAPE(todolist);
+var DOM = _DOM_SCRAPE(todolist);
 
 /* var DOM = _DOM_SCRAPE(todolist);
 function ring(){
