66b1ae3368daef3d0edcb5d567ba78f6ca48678f
Moonsung <gmsj159@gmail.com>
2020-11-19 17:49:43 +1000

    react initialized

---
+++ reactjs/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        Hello, React!!<br></br>
+        Australia is good!!!<br></br>
+        Omok!
+      </div>
+    );
+  }
+}
+
+export default App;
